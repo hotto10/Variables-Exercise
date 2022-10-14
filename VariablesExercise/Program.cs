c1dfb65a0ca930b71ebff9d5fb98fb5c1485b961
heather otto <heatherotto32@yahoo.com>
2022-10-14 15:00:13 -0500

    initial commit

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string myFirstName = "Heather";
+            int myAge = 33;
+            char middleInitial = 'M';
+            bool x = true;
+            double myWeight = 130;
+            decimal myWeight2 = 125;
+
+            Console.WriteLine($"My first name is {myFirstName}, I am {myAge} years old");
+            Console.WriteLine($"My middle name is {middleInitial} and this is {x} in all instances");
+            Console.WriteLine($"My weight fluctuates from {myWeight2} to {myWeight} every day");
+
         }
     }
 }
